805a8a4934e0e4e4ad687f1cbc93fa67954ee6a8
Lorito Tiago <58330997+LoritoTiago@users.noreply.github.com>
2022-02-28 22:53:24 +0100

    Set animation

--- widget_of_the_week_2/lib/view/home.dart
+++ widget_of_the_week_2/lib/view/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,11 +16,16 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     _lottie = AnimationController(vsync: this);
+    Future.delayed(const Duration(seconds: 5), () {
+      isFinish = true;
+      setState(() {});
+    });
     _lottie!.addListener(() {
-      if (_lottie!.value > 0.2) {
+      if (_lottie!.value > 0.9) {
         _lottie!.stop();
+        isFinish = true;
         setState(() {});
-        Future.delayed(const Duration(seconds: 2), () {
+        Future.delayed(const Duration(milliseconds: 500), () {
           shoWText = true;
           setState(() {});
         });
@@ -28,7 +34,130 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _lottie!.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var h = MediaQuery.of(context).size.height,
+        w = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            child: Container(
+              color: const Color(0xff4D7321),
+              width: w,
+              height: h / 2.6,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 50),
+                    const Text(
+                      "We bring your product to you",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Lorito Deliveries is a non-real world company used for testing.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(20.0),
+                          child: const Icon(
+                            Icons.arrow_forward_ios_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            child: AnimatedContainer(
+              height: isFinish ? h / 1.5 : h,
+              width: w,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(isFinish ? 20 : 0),
+                  bottomRight: Radius.circular(isFinish ? 20 : 0),
+                ),
+              ),
+              duration: const Duration(seconds: 1),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Visibility(
+                    visible: isFinish,
+                    child: LottieBuilder.asset(
+                      "assets/animation/food.json",
+                      controller: _lottie,
+                      onLoaded: (lot) {
+                        _lottie!
+                          ..duration = lot.duration
+                          ..forward();
+                      },
+                    ),
+                  ),
+                  Visibility(
+                    visible: !isFinish,
+                    child: LottieBuilder.asset(
+                      "assets/animation/food.json",
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: AnimatedOpacity(
+                      duration: const Duration(seconds: 1),
+                      opacity: shoWText ? 1 : 0,
+                      child: const Text(
+                        "Delivery",
+                        style: TextStyle(
+                          color: Color(
+                            0xff4D7321,
+                          ),
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
